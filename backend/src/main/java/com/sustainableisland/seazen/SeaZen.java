32b1a640846caca3e450c565249969aa78920620
DESKTOP-CD3B06E\JuanAntonio <ja.sanchezmartel@gmail.com>
2026-03-04 18:13:21 +0100

    Fixed more issues and discrepances.

--- backend/src/main/java/com/sustainableisland/seazen/SeaZen.java
+++ backend/src/main/java/com/sustainableisland/seazen/SeaZen.java
@@ -1,19 +1,13 @@
-package com.sustainableisland.backend;
+package com.sustainableisland.seazen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.sustainableisland")
-@EnableJpaRepositories(basePackages = "com.sustainableisland")
-@EntityScan(basePackages = "com.sustainableisland")
 public class SeaZen {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeaZen.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SeaZen.class, args);
+    }
 
 }
--- backend/src/main/java/com/sustainableisland/seazen/models/User.java
+++ backend/src/main/java/com/sustainableisland/seazen/models/User.java
@@ -4,13 +4,11 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.util.Set;
 
 @Entity
-@Table(name = "users", 
-    uniqueConstraints = { 
-      @UniqueConstraint(columnNames = "email") 
-    })
+@Table(name = "users", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "email")
+})
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
