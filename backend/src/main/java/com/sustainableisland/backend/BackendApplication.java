cb8c47388075485245813ae731cb79c1f769ea34
DESKTOP-CD3B06E\JuanAntonio <ja.sanchezmartel@gmail.com>
2026-03-04 19:59:36 +0100

    JWTAuth implemented v0.1

--- backend/src/main/java/com/sustainableisland/backend/BackendApplication.java
+++ backend/src/main/java/com/sustainableisland/backend/BackendApplication.java
@@ -2,8 +2,15 @@ package com.sustainableisland.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"com.sustainableisland.backend",
+		"com.sustainableisland.seazen"
+})
+@EnableJpaRepositories(basePackages = "com.sustainableisland.seazen.repositories")
+@EntityScan(basePackages = "com.sustainableisland.seazen.models")
 public class BackendApplication {
 
 	public static void main(String[] args) {
