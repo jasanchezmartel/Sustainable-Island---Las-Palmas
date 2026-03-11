838878051762ac0d4e6a09a829717a8faf477c8e
Oliver-Bredmose <olbr1@elev.techcollege.dk>
2026-03-11 10:46:38 +0000

    this is Issue nr 9 user-profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router"
 import { Login } from "./pages/Login/Login"
-import { LandingPage } from "./pages/landingPage"
+import { LandingPage } from "./pages/LandingPage/landingPage"
 import { Signup } from "./pages/Signup/Signup"
 import { NavBar } from "./components/NavBar/NavBar"
+import { UserProfile } from "./pages/User Profile/userProfile"
+import { MissionsPage } from "./pages/Missions/MissionsPage"
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path='/' element={<LandingPage />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/userProfile' element={<UserProfile />} />
+          <Route path='/missions' element={<MissionsPage />} />
         </Routes>
       </BrowserRouter>
 
---
+++ frontend/src/pages/User Profile/UserProfile.tsx
@@ -0,0 +1,11 @@
+import style from "../../pages/User Profile/userProfile.module.scss";
+
+
+export const UserProfile = () => {
+  return (
+    <div className={style.userProfile}>
+      <h1>User Profile</h1>
+      <p>This is the user profile page.</p>
+    </div>
+  );
+};
