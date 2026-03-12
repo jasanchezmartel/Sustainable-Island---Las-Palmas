d96ea1f5148efebf4efb4d5feb871f852daf5c5e
Oliver-Bredmose <olbr1@elev.techcollege.dk>
2026-03-12 10:53:15 +0000

    fix things before mirandas last updates

--- frontend/src/pages/User Profile/UserProfile.tsx
+++ frontend/src/pages/User Profile/UserProfile.tsx
@@ -1,11 +1,30 @@
 import style from "../../pages/User Profile/userProfile.module.scss";
 
+export const UserProfile = ({status = 90}) => {
+  console.log({status})
 
-export const UserProfile = () => {
+  let img = '';
+
+  if(status < 39) {
+    img = 'sad-seal.png';
+  } else if (status < 79) {
+    img = 'neutral-seal.png';
+  } else {
+    img = 'happy-seal.png';
+  }
+
+
   return (
     <div className={style.userProfile}>
       <h1>User Profile</h1>
       <p>This is the user profile page.</p>
+      <div>
+        <img src={img} />
+      </div>
+      <div className={style.statusBar}>
+        <div className={style.progressBar} style={{width: `${status}%`}}></div>
+
+      </div>
     </div>
   );
 };
